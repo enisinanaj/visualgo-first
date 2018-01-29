2680a08ae5436a1a4d3c85e05c0dc60a50f2d59b
Eni Sinanaj <enisinanaj@outlook.com>
2018-01-29 02:29:01 +0100

    android project for detached expo app

---
+++ android/app/src/main/java/com/areak/VisualGo/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package com.areak.VisualGo.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "";
+
+}
