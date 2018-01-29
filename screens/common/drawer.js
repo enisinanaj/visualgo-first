9390812c37835663e0269c5f60deb928c99a6b47
kevor <cosimo.mabilia@gmail.com>
2018-01-29 22:38:57 +0100

    Added menu items

--- screens/common/drawer.js
+++ screens/common/drawer.js
@@ -7,14 +7,27 @@ import {
     Dimensions,
     ListView,
     Text,
-    StyleSheet
+    StyleSheet,
+    Image
 } from 'react-native';
 
 const {width, height} = Dimensions.get('window');
+import {EvilIcons} from '@expo/vector-icons';
 import Colors from '../../constants/Colors';
 import ChatSearchBar from './chat-search-bar';
+import DefaultRow from '../common/default-row';
 
-const data = ['1', '1', '1', '1', '1', '1', '1', '1', '1'];
+const data = ['Report', 'Visual Guideline', 'Wall', 'Calendar', 'Messages', 'To Do List', ' ', 'Anagrafiche', 'Logout'];
+
+const menus = [ {name: 'Report', image: require('../img/elmo.jpg')},
+                {name: 'Visual Guideline', image: require('../img/elmo.jpg')},
+                {name: 'Wall', image: require('../img/elmo.jpg')},
+                {name: 'Calendar', image: require('../img/elmo.jpg')},
+                {name: 'Messages', image: require('../img/elmo.jpg')},
+                {name: 'To Do List', image: require('../img/elmo.jpg')} ];
+
+
+
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
 export default class Drawer extends Component {
@@ -26,16 +39,24 @@ export default class Drawer extends Component {
     }
 
     _renderRow(data) {
-        return (
-            <View></View>
-        )
+        return <DefaultRow arguments={data} noborder={true} renderChildren={() => this.renderMenuItem(data)}/>
+    }
+
+    renderMenuItem(data){
+
+            return (<Text style={styles.menuItem}>{data}</Text>)
+        
     }
 
 
     render() {
         return (
             <View style={styles.drawer}>
-                
+                <Image source={require('../img/elmo.jpg')} style={styles.selectableDisplayPicture} />
+                <Text style={styles.accountName}>Giannino De Gianni</Text>
+                <Text style={styles.accountEmail}>giannino@warda.it</Text>
+
+
                 <ListView
 
                     dataSource={this.state.dataSource}
@@ -52,8 +73,37 @@ const styles = StyleSheet.create({
         height,
         paddingTop: 20,
         width: width * 4/5,
+        paddingLeft: 40,
         position: 'absolute',
         backgroundColor: Colors.main,
         right: 0
+    },
+
+    selectableDisplayPicture: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginTop: 20,
+        marginBottom: 10,
+    },
+
+    menuItem: {
+        color: Colors.white,
+        fontSize: 22,
+        paddingLeft: 40,
+        
+    },
+
+    accountName: {
+        color: Colors.white,
+        
+        fontSize: 22
+    },
+
+    accountEmail: {
+        color: Colors.white,
+        fontSize: 18,
+        marginBottom: 30
+
     }
 });
