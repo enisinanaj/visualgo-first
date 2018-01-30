b08cd6bf70ddf9e3b9734d1362d1eaddb4c233a7
Eni Sinanaj <enisinanaj@outlook.com>
2018-01-30 01:44:08 +0100

    Converted to react native with ExpoTooltik!

--- android/app/src/main/java/com/areak/VisualGo/generated/ExponentBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package com.areak.VisualGo.generated;
-
-// This file is auto-generated. Please don't rename!
-public class ExponentBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "";
-
-}
