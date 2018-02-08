56e1ed1d2fe04f6da75a64c934ae778458ae27cf
Eni Sinanaj <enisinanaj@outlook.com>
2018-02-09 00:52:26 +0100

    Fixes in login view (at least for iPhone)

--- screens/Login.js
+++ screens/Login.js
@@ -21,6 +21,7 @@ import {
 import Drawer from 'react-native-drawer'
 
 import Colors from '../constants/Colors';
+import Shadow from '../constants/Shadow';
 import SearchBar from './common/search-bar';
 import DefaultRow from './common/default-row';
 import FilterBar from './common/filter-bar';
@@ -49,9 +50,9 @@ export default class Login extends Component {
         emailTyped: false,
         email: '',
         pass: '',
+        showPasswordLabel: 'Show password',
+        secureEntryPassword: true
     };
-
-
   }
 
   _goToLanding = (messageId) => {
@@ -61,61 +62,40 @@ export default class Login extends Component {
   _renderHeader() {
     return (
         <View style={styles.headerView}>
-            
             <View style={{flexDirection: 'row', justifyContent: 'center', width: width}}>
                 <Text style={styles.viewTitle}>Login</Text>
-                
             </View>
         </View>);
   }
 
   emailChanged(email) {
-
     if(email != ''){
       this.state.emailTyped = true;
     }else{
       this.state.emailTyped = false;
     }
 
-
     if(this.state.passTyped && this.state.emailTyped){
       this.state.canLogin = true;
-      this._buttonLogin.setNativeProps({style: styles.buttonLoginEnabled});
-
       this.setState({canLogin: true});
-      //this._buttonLogin.setNativeProps({disabled: false});
-
     }else{
-      this._buttonLogin.setNativeProps({style: styles.buttonLoginDisabled});
-
       this.setState({canLogin: false});
     }
-
-
   }
 
   passwordChanged(pass) {
-    
-
     if(pass != ''){
       this.state.passTyped = true;
     }else{
       this.state.passTyped = false;
     }
 
-    if(this.state.emailTyped && this.state.passTyped){
+    if (this.state.emailTyped && this.state.passTyped) {
       this.state.canLogin = true;
-      this._buttonLogin.setNativeProps({style: styles.buttonLoginEnabled});
       this.setState({canLogin: true});
-      //this._buttonLogin.setNativeProps({disabled: false});
-
-    }else{
-      this._buttonLogin.setNativeProps({style: styles.buttonLoginDisabled});
+    } else {
       this.setState({canLogin: true});
     }
-
-    
-
   }
 
   LogIn(){
@@ -123,7 +103,13 @@ export default class Login extends Component {
   }
 
   showPassword(){
-    this._passInput.setNativeProps({secureTextEntry: false});
+    if (this.state.secureEntryPassword) {
+      this._passInput.setNativeProps({secureTextEntry: false});
+      this.setState({showPasswordLabel: 'Hide password', secureEntryPassword: false});
+    } else {
+      this._passInput.setNativeProps({secureTextEntry: true});
+      this.setState({showPasswordLabel: 'Show password', secureEntryPassword: true});      
+    }
   }
 
   render() {
@@ -131,55 +117,57 @@ export default class Login extends Component {
         return (
           <KeyboardAvoidingView style={{flex: 1, height: visibleHeight}} behavior={"padding"}>
             <Animated.View style={[Platform.OS === "ios" ? styles.containerIOS : styles.containerAndroid, {height}]}/>
-            <ScrollView>
+
             <View style={{flexDirection: 'column', backgroundColor: Colors.white}} resetScrollToCoords={{x: 0, y: 0}}>
-
-
-
+              <ScrollView>
                           
-                          <DefaultRow renderChildren={() => this._renderHeader()} />
+                <DefaultRow renderChildren={() => this._renderHeader()} />
 
-                          <TouchableOpacity style={styles.buttonStyleLinkedin}>
-                            <Text style={styles.buttonContentStyleLinkedin}>LOGIN WITH LINKEDIN</Text>
-                          </TouchableOpacity>
+                <TouchableOpacity>
+                  <View style={[styles.oAuthButton, styles.buttonStyleLinkedin, Shadow.filterShadow]}>
+                    <Text style={[styles.oAuthButtonContent, Platform.OS == 'ios' ? styles.loginOauth : {} ]}>LOGIN WITH LINKEDIN</Text>
+                  </View>
+                </TouchableOpacity>
 
-                          <TouchableOpacity style={styles.buttonStyleGoogle}>
-                            <Text style={styles.buttonContentStyleGoogle}>LOGIN WITH FACEBOOK</Text>
-                          </TouchableOpacity>
+                <TouchableOpacity>
+                  <View style={[styles.oAuthButton, styles.buttonStyleGoogle, Shadow.filterShadow]}>
+                    <Text style={[styles.oAuthButtonContent, Platform.OS == 'ios' ? styles.loginOauth : {} ]}>LOGIN WITH GOOGLE</Text>
+                  </View>
+                </TouchableOpacity>
 
-                          <Text style={styles.OrText}>Or... </Text>
+                <Text style={styles.OrText}>Or... </Text>
 
-                          <Text style={styles.grayText}>Enter your e-mail address </Text>
+                <Text style={styles.grayText}>Enter your e-mail address </Text>
 
-                          <View style={styles.searchBarContainer}>
-                            <TextInput ref={component => this._emailInput = component} placeholderTextColor={Colors.main} placeholder={'Email'} style={styles.searchBar} onChangeText={(email) => this.emailChanged(email)}/>
-                          </View>
+                <View style={[styles.textField]}>
+                  <TextInput ref={component => this._emailInput = component} placeholderTextColor={Colors.main} placeholder={'Email'} 
+                    style={styles.textFieldContent} onChangeText={(email) => this.emailChanged(email)}/>
+                </View>
 
-                          <Text style={styles.grayText}>Enter your password </Text>
+                <Text style={styles.grayText}>Enter your password </Text>
 
-                          <View style={styles.searchBarContainer}>
-                            <TextInput ref={component => this._passInput = component} secureTextEntry={true} placeholderTextColor={Colors.main} placeholder={'Password'} style={styles.searchBar} onChangeText={(pass) => this.passwordChanged(pass)}/>
-                          </View>
+                <View style={[styles.textField]}>
+                  <TextInput ref={component => this._passInput = component} secureTextEntry={true} placeholderTextColor={Colors.main} placeholder={'Password'} 
+                    style={styles.textFieldContent} onChangeText={(pass) => this.passwordChanged(pass)}/>
+                </View>
 
-                          <TouchableOpacity onPress={() => this.showPassword()}>
-                            <Text style={styles.ShowPasswordText}>Show Password</Text>
-                          </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.showPassword()}>
+                  <Text style={styles.ShowPasswordText}>{this.state.showPasswordLabel}</Text>
+                </TouchableOpacity>
 
-                          <TouchableOpacity ref={component => this._buttonLogin = component} disabled={!this.state.canLogin} style={styles.buttonLoginDisabled} onPress={() => this.LogIn()}>
-                            <Text style={styles.buttonContentStyleGoogle}>LOGIN</Text>
-                          </TouchableOpacity>
+                <TouchableOpacity ref={component => this._buttonLogin = component} disabled={!this.state.canLogin} onPress={() => this.LogIn()}>
+                  <View style={[styles.buttonLoginDisabled, styles.oAuthButton, Shadow.filterShadow]}>
+                    <Text style={[styles.oAuthButtonContent, styles.loginText]}>LOGIN</Text>
+                  </View>
+                </TouchableOpacity>
 
-                          <TouchableOpacity>
-                            <Text style={styles.ForgottenText}>Forgotten your password?</Text>
-                          </TouchableOpacity>
+                <TouchableOpacity>
+                  <Text style={styles.ForgottenText}>Forgotten your password?</Text>
+                </TouchableOpacity>
 
-                 
-
-                  
-            
+              </ScrollView>
             
             </View>
-            </ScrollView>
             
           </KeyboardAvoidingView>
 
@@ -193,40 +181,43 @@ const drawerStyles = {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-    },
-    containerIOS: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      height: 60,
-      backgroundColor: Colors.main,
-      paddingTop: 20,
-      borderBottomColor: Colors.borderGray,
-      borderBottomWidth: 1,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
   },
 
-  buttonContentStyleLinkedin: {
+  containerIOS: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 60,
+    backgroundColor: Colors.main,
+    paddingTop: 20,
+    borderBottomColor: Colors.borderGray,
+    borderBottomWidth: 1,
+  },
 
+  containerAndroid:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 64,
+    backgroundColor: Colors.main,
+    paddingTop: 24,
+    borderBottomColor: Colors.borderGray,
+    borderBottomWidth: 1,
+  },
+
+  oAuthButtonContent: {
     textAlign: 'center',
     color: Colors.white,
     fontSize: 20,
     paddingBottom: 10,
     fontWeight: '100',
-},
+    paddingTop: 5
+  },
 
-buttonContentStyleGoogle: {
-
-  textAlign: 'justify',
-  color: Colors.white,
-  fontSize: 20,
-  paddingBottom: 10,
-  fontWeight: '100',
-},
-
-  buttonStyleLinkedin: {
+  oAuthButton: {
     flexDirection: 'column',
     borderRadius: 25,
     padding: 10,
@@ -235,181 +226,93 @@ buttonContentStyleGoogle: {
     marginLeft: 40, 
     minWidth: 75,
     marginTop: 20,
-  
-    backgroundColor: '#2F77B0',
     alignItems: 'center'
-    //fontFamily: 'Roboto-Light'
-},
-
-buttonStyleGoogle: {
-  flexDirection: 'column',
-  borderRadius: 25,
-  padding: 10,
-  height: 50,
-  marginRight: 40,
-  marginLeft: 40,
-  minWidth: 75,
-  marginTop: 20,
-  
-  backgroundColor: '#4F86EC',
-  alignItems: 'center'
-  //fontFamily: 'Roboto-Light'
-},
-
-buttonLoginEnabled: {
-  flexDirection: 'column',
-  borderRadius: 25,
-  padding: 10,
-  height: 50,
-  marginRight: 40,
-  marginLeft: 40,
-  minWidth: 75,
-  marginTop: 20,
-  
-  backgroundColor: '#4F86EC',
-  alignItems: 'center'
-  //fontFamily: 'Roboto-Light'
-},
-
-buttonLoginDisabled: {
-  flexDirection: 'column',
-  borderRadius: 25,
-  padding: 10,
-  height: 50,
-  marginRight: 40,
-  marginLeft: 40,
-  minWidth: 75,
-  marginTop: 20,
-  
-  backgroundColor: Colors.gray,
-  alignItems: 'center'
-  //fontFamily: 'Roboto-Light'
-},
-
-  containerAndroid:{
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      height: 64,
-      backgroundColor: Colors.main,
-      paddingTop: 24,
-      borderBottomColor: Colors.borderGray,
-      borderBottomWidth: 1,
   },
-    viewTitle: {
-      fontSize: 20,
-      fontWeight: '800',
-      color: Colors.main,
-      justifyContent: 'center',
-    },
 
-    OrText:{
-      fontSize: 16,
-      fontWeight: '800',
-      color: Colors.main,
-      marginLeft: 40,
-      marginTop: 30,
-
-    },
-
-    ShowPasswordText:{
-      fontSize: 16,
-      fontWeight: '800',
-      color: Colors.main,
-      marginLeft: 40,
-      marginTop: 0,
-
-    },
-
-    ForgottenText:{
-      fontSize: 16,
-      fontWeight: '800',
-      color: '#FBD54A',
-      marginLeft: 40,
-      marginTop: 20,
-
-    },
-
-    grayText:{
-      fontSize: 14,
-      fontWeight: '100',
-      color: Colors.gray,
-      marginLeft: 40,
-      marginTop: 30,
-
-    },
-
-    headerView: {
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'stretch',
-      paddingTop: 5,
-      paddingBottom: 5,
-      
+  buttonStyleLinkedin: {
+      backgroundColor: '#2F77B0'
   },
-    selectableDisplayPicture: {
-        width: 50,
-        height: 50,
-        borderRadius: 25
-    },
-    unreadMessage: {
-        fontWeight: '800'
-    },
-    modalContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      backgroundColor: 'grey',
-    },
-    innerContainer: {
-      alignItems: 'center',
-    },
-    rowContainer: {
-      padding: 5,
-      flex: 1,
-      flexDirection: 'row'
-    },
-    textInRow: {
-      marginLeft: 10,
-      marginTop: 5,
-      flex: 1,
-      flexDirection: 'column',
-    },
-    rowTitle: {
-      fontWeight: '400',
-      fontSize: 18
-    },
-    rowSubTitle: {
-      color: Colors.grayText,
-      fontSize: 14
-    },
-    filterBarContainer: {
-        backgroundColor: Colors.white,
-        
-    },
-    messageDate: {
-        paddingTop: 17
-    },
-    listView: {
-      backgroundColor: Colors.white,
-      flexDirection: 'column'
-      
-    },
-    searchBarContainer: {
-      alignItems: 'center',
-      height: 60,
-      marginLeft: 40,
-      flexDirection: 'column',
-      marginRight: 40,
-      marginLeft: 40, 
-      minWidth: 75,
-    },
-    searchBar: {
-      flex: 1,
-      color: Colors.main,
-      fontSize: 28,
-      marginLeft: 0,
-      paddingBottom: 8,
-      width: 314,
-    }
-  });
+
+  buttonStyleGoogle: {
+    backgroundColor: '#4F86EC'
+  },
+
+  buttonLoginEnabled: {
+    backgroundColor: Colors.main,
+  },
+
+  buttonLoginDisabled: {
+    backgroundColor: Colors.borderGray,
+  },
+
+  loginText: {
+    color: Colors.black
+  },
+
+  textField: {
+    marginLeft: 40,
+    marginRight: 40,
+    borderBottomColor: Colors.grayText,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    paddingBottom: 5,
+    paddingTop: 5,
+    height: 55,
+    marginTop: 0,
+    marginBottom: 5
+  },
+
+  textFieldContent: {
+    fontSize: 52,
+    fontWeight: '100'
+  },
+
+  viewTitle: {
+    fontSize: 20,
+    fontWeight: '800',
+    color: Colors.main,
+    justifyContent: 'center',
+  },
+
+  OrText:{
+    fontSize: 16,
+    fontWeight: '800',
+    color: Colors.main,
+    marginLeft: 40,
+    marginTop: 20,
+  },
+
+  grayText:{
+    fontSize: 14,
+    fontWeight: '100',
+    color: Colors.grayText,
+    marginLeft: 40,
+    marginTop: 20,
+  },
+
+  ShowPasswordText:{
+    fontSize: 16,
+    fontWeight: '800',
+    color: Colors.main,
+    marginLeft: 40,
+    marginTop: 0,
+    marginBottom: 15
+  },
+
+  ForgottenText:{
+    fontSize: 16,
+    fontWeight: '800',
+    color: Colors.yellow,
+    marginLeft: 40,
+    marginTop: 25,
+    marginBottom: 30
+  },
+
+  headerView: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    paddingTop: 5,
+    paddingBottom: 5,   
+  }
+});
